4a4f6532e7cf92c57f1a2fb3d62c79e683e430c7
scallop200603 <21020549@vnu.edu.vn>
2022-05-25 11:21:23 +0700

    Header file cpp

--- header.cpp
+++ header.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void logSDLError(ostream& os, const string &msg, bool fatal)
+void logSDLError(const string &msg, bool fatal)
 {
      os << msg << " Error: " << SDL_GetError() << endl;
      if (fatal)
@@ -19,15 +19,15 @@ void logSDLError(ostream& os, const string &msg, bool fatal)
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer, const int SCREEN_WIDTH, const int SCREEN_HEIGHT,const string WINDOW_TITLE)
 {
      if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-        logSDLError(cout, "SDL_Init", true);
+        logSDLError("SDL_Init", true);
      window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
      //window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
-     if (window == nullptr) logSDLError(cout, "CreateWindow", true);
+     if (window == nullptr) logSDLError("CreateWindow", true);
      //Khi thông thường chạy với môi trường bình thường ở nhà
      renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
      //Khi chạy ở máy thực hành WinXP ở trường (máy ảo)
         //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
-     if (renderer == nullptr) logSDLError(cout, "CreateRenderer", true);
+     if (renderer == nullptr) logSDLError("CreateRenderer", true);
      SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
      SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
